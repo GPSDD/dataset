29ad8a6459792371a53e3fcaaa32e30e35495fad
manuelnelson <elnels@gmail.com>
2018-12-04 17:22:03 -0500

    moving to config

--- app/src/services/mail.service.js
+++ app/src/services/mail.service.js
@@ -1,37 +1,38 @@
 const SparkPost = require('sparkpost');
 const Promise = require('bluebird');
 const logger = require('logger');
-
-const sparkpostKey = process.env.SPARKPOST_API_KEY;
-logger.debug('sparkpostKey ', sparkpostKey);
-const Client = new SparkPost(sparkpostKey);
+const config = require('config');
 
 class MailService {
+  constructor() {
+    logger.debug('sparkpostKey ', sparkpostKey);
+    this.client = new SparkPost(config.get('sparkpost'));
+  }
 
-   static async sendDatasetMail(data, recipients) {
-        logger.debug('Sending dataset created to ', recipients);
-        const reqOpts = {
-            transmissionBody: {
-                substitution_data: {
-                    name: `${data.name}`,
-                    id: `${data.id}`,
-                },
-                content: {
-                    template_id: 'dataset-created',
-                },
-                recipients,
-            },
-        };
-        return new Promise((resolve, reject) => {
-          logger.debug(reqOpts);
-            Client.transmissions.send(reqOpts, (error, res) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(res);
-                }
-            });
-        });
-    }
+  static async sendDatasetMail(data, recipients) {
+      logger.debug('Sending dataset created to ', recipients);
+      const reqOpts = {
+          transmissionBody: {
+              substitution_data: {
+                  name: `${data.name}`,
+                  id: `${data.id}`,
+              },
+              content: {
+                  template_id: 'dataset-created',
+              },
+              recipients,
+          },
+      };
+      return new Promise((resolve, reject) => {
+        logger.debug(reqOpts);
+          this.client.transmissions.send(reqOpts, (error, res) => {
+              if (error) {
+                  reject(error);
+              } else {
+                  resolve(res);
+              }
+          });
+      });
+  }
 }
 module.exports = MailService;
